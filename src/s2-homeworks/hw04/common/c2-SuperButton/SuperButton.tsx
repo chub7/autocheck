f8e92ac66d9b218f59378be30e22db4432312f0a
Dima Godunov <gandiggpk@gmail.com>
2022-10-14 13:42:36 +0300

    14 commit

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,7 +17,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    debugger
+
     const finalClassName = s.button + ' '
         + (disabled ? s.disabled :
             xType === 'red' ? s.red :
--- src/s2-homeworks/hw05/pages/Error404.tsx
+++ src/s2-homeworks/hw05/pages/Error404.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import s from './Error404.module.css'
 import error404 from './404.svg'
+import {NavLink} from "react-router-dom";
+
 const Error404 = () => {
     return (
+
         <div id={'hw5-page-404'}>
+
             <div className={s.wrapper}>
                 <img src={error404} alt={'404'} className={s.error404} />
             </div>
         </div>
+
     )
 }
 
