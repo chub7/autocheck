450f667b37f58c54c1fcd42320433bf8f606c810
Dima Godunov <gandiggpk@gmail.com>
2022-11-02 22:43:15 +0300

    7hw

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,16 +4,30 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+           if(action.payload === 'up') {
+               return [...state.sort((a, b) => {
+                   if (a.name > b.name) {
+                       return 1
+                   } else return -1
+               })]
+           } else if (action.payload === 'down') {
+               return [...state.sort((a, b) => {
+                   if (a.name < b.name) {
+                       return 1
+                   } else return -1
+               })]
+           } else return state
 
-            return state // need to fix
+           // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return [...state.filter(e=>e.age>=18)]
         }
+
         default:
             return state
     }
